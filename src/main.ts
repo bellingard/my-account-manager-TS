54c1a5ddfb26b9926c46ee4eba4e488323907558
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2019-01-06 17:56:18 +0100

    Adding Vue plugin Vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
