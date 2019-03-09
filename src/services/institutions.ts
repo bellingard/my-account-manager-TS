e2a8aa6829da4fbcb23ae7f81caefdbdc86fa5bf
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2019-03-09 17:21:39 +0100

    Adding Institutions

--- src/services/institutions.ts
+++ src/services/institutions.ts
@@ -1,4 +1,31 @@
+import * as _ from 'lodash'
+import Storage from './utils/storage';
+
 export interface Institution {
   id: string
   name: string
+}
+
+export class Institutions {
+  private storage: Storage
+
+  constructor(storage: Storage) {
+    this.storage = storage
+  }
+
+  /**
+   * Returns the list of institutions
+   */
+  list(): Institution[] {
+    return _.values(this.storage.repo().institutions)
+  }
+
+  /**
+   * Returns the institution for the given ID if it exists.
+   * @param id
+   */
+  get(id: string): Institution | undefined {
+    return this.storage.repo().institutions[id]
+  }
+
 }
---
+++ tests/unit/services/institutions.spec.ts
@@ -0,0 +1,34 @@
+import Storage from '@/services/utils/storage'
+import { Institutions } from '@/services/institutions'
+
+// Mock Storage
+const mockRepo = {
+  institutions: {
+    I303: { id: 'I303', name: 'Institution 303' },
+    I424: { id: 'I424', name: 'Institution 424' },
+    I545: { id: 'I545', name: 'Institution 545' }
+  }
+}
+jest.mock('@/services/utils/storage', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      repo: () => {
+        return mockRepo
+      }
+    }
+  })
+})
+
+// And start the tests
+const institutions = new Institutions(new Storage({ storageFolder: '' }))
+
+describe('Institutions', () => {
+  it('should list institutions', () => {
+    expect(institutions.list().length).toEqual(3)
+  })
+
+  it('should get institution from ID', () => {
+    expect(institutions.get('I303')).toEqual({ id: 'I303', name: 'Institution 303' })
+    expect(institutions.get('unknown_ID')).toBeUndefined()
+  })
+})
