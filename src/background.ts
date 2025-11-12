d836d2a9d68276d79bf0a44be71f0c6e2f06efb0
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2025-11-12 09:02:07 +0100

    Add another issue

--- src/background.ts
+++ src/background.ts
@@ -11,7 +11,7 @@ let win: BrowserWindow | null
 // Standard scheme must be registered before the app is ready
 protocol.registerStandardSchemes(['app'], { secure: true })
 function createWindow() {
-  let foo = ""
+  console.log("blah")
   
   // Create the browser window.
   win = new BrowserWindow({ width: 800, height: 600 })
